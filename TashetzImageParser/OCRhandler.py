476692a89af30a6be20b9e4d8df6eca3312e96c9
OrezriceWasHere <orezrice@gmail.com>
2018-06-16 13:43:35 +0300

    update OCR Handler

--- TashetzImageParser/OCRhandler.py
+++ TashetzImageParser/OCRhandler.py
@@ -1,6 +1,6 @@
 import requests
 import ImageHandler
-
+import Settings
 
 def parseTashets(image_bytes=None, image_locaion=None):
 
@@ -8,7 +8,8 @@ def parseTashets(image_bytes=None, image_locaion=None):
         raise ValueError("Need to have at least one parameter")
 
 
-    url = 'https://content-vision.googleapis.com/v1/images:annotate?key=AIzaSyA---60QEj1665wtX-iEJ0YSeDZR4dRvmw&alt=json'
+    url = 'https://content-vision.googleapis.com/v1/images:annotate?key={google_key}&alt=json'\
+        .format(google_key = Settings.google_api_key)
 
     base64_encodedImage = ImageHandler.handleImage(image_locaion) if image_bytes is None else image_bytes
 
